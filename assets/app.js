2cd5914c94edbfa795c33a195c7308a7b2dfe8aa
Mickael Tailhades <87366929+RyuuCreator@users.noreply.github.com>
2022-06-22 11:59:23 +0200

    update_v2.5

--- assets/app.js
+++ assets/app.js
@@ -29,14 +29,7 @@ import './js/slide-cat-amb';
 const navigation = document.getElementById("nav");
 const menu = document.getElementById("menu");
 
-menu.addEventListener("click", () => {
-  // The navigation.children.length means the following :-
-  // The children inside a parent are basically an array of elements; So, here I'm finding the length of the array aka how many children are inside the nav bar.
-  //   Yup That's all.
-    navigation.style.setProperty("--childenNumber", navigation.children.length);
-
-  //    Casually Toggling Classes to make them animate on click
-  //   Regular stuff ;)
+menu.addEventListener("click", () => {    
     navigation.classList.toggle("active");
     menu.classList.toggle("active");
 });
